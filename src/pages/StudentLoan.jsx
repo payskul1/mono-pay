fe31c8abc7b42df086a3182e65e376fd46e44e15
Niola-DC <eniolachinemerem74@gmail.com>
2025-07-07 10:39:26 -0500

    test keys added

--- src/pages/StudentLoan.jsx
+++ src/pages/StudentLoan.jsx
@@ -483,7 +483,8 @@ const Financial = ({ formData, handleInputChange }) => (
 
 const BankAccount = ({ formData, handleInputChange, bankConnected, handleSuccess, handleError, handleClose }) => {
     // const pubKey = "test_pk_vulwcz9yw9kqdtvua5q4";
-    const pubKey = "live_pk_g8pqod3pkbkwds5mmi2i";
+    // const pubKey = "live_pk_g8pqod3pkbkwds5mmi2i";
+    const pubKey = "test_pk_ohr9l7gksqibkvlavjyn"
     const customerName = formData.firstName + formData.lastName;
     const customerEmail = formData.email;
     //   const [bvn, set]
